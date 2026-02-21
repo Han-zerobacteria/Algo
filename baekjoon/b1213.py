fe37af6078dbb94dff4d3129f24e2f795514bce0
한영균 <hyg9112@naver.com>
2026-02-21 17:43:25 +0900

    .

---
+++ baekjoon/b1213.py
@@ -0,0 +1,28 @@
+name = input().strip()
+
+# 1. 문자 개수 세기
+count = {}
+
+for ch in name:
+    if ch in count:
+        count[ch] += 1
+    else:
+        count[ch] = 1
+
+odd_count = 0
+mid = ""
+left = ""
+
+# 2. 알파벳 순으로 처리
+for ch in sorted(count.keys()):
+    if count[ch] % 2 == 1:
+        odd_count += 1
+        mid = ch
+    
+    left += ch * (count[ch] // 2)
+    print(count)
+# 3. 팰린드롬 가능 여부 판단
+if odd_count > 1:
+    print("I'm Sorry Hansoo")
+else:
+    print(left + mid + left[::-1])
